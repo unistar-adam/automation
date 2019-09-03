686b2b9f0f730c071c6348778b5993e5683b1178
unistar-adam <adam.chen@yxunistar.com>
2019-09-03 15:02:21 +0800

    update python for loop lab

--- python/lab/Loop/for_1.py
+++ python/lab/Loop/for_1.py
@@ -5,10 +5,10 @@
 #    statements(s)
  
 for letter in 'Python':     # 第一个实例
-   print '当前字母 :', letter
+   print ('当前字母 :', letter)
  
 fruits = ['banana', 'apple',  'mango']
 for fruit in fruits:        # 第二个实例
-   print '当前水果 :', fruit
+   print ('当前水果 :', fruit)
  
-print "Good bye!"
+print ("Good bye!")
