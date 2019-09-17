40ff53ce92b2238f7a98ed5d42eade029f82471b
unistar-adam <adam.chen@yxunistar.com>
2019-09-17 18:00:34 +0800

    Update Bind DNS

--- python/lab/Loop/for_1.py
+++ python/lab/Loop/for_1.py
@@ -4,11 +4,10 @@
 # for iterating_var in sequence:
 #    statements(s)
  
-for letter in 'Python':     # 第一个实例
-   print ('当前字母 :', letter)
- 
+# for letter in range(1,5):     # 第一个实例
+#    print ('当前字母 :', letter)
+
 fruits = ['banana', 'apple',  'mango']
 for fruit in fruits:        # 第二个实例
    print ('当前水果 :', fruit)
- 
 print ("Good bye!")
--- python/lab/Loop/while_1.py
+++ python/lab/Loop/while_1.py
@@ -1,10 +1,11 @@
 import random
 ans =  random.randint(0,10) #取從1到10的亂數
-
+print(ans)
 print('請猜一個0~10的數字')
 play = True
 while(play):
     guess = int(input('來隨便猜一個數字吧: ')) #input出來預設為str，轉型為int
+    
     if (guess > ans):
         print('喔你猜得太大囉')
     elif (guess < ans):
